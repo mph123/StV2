40522d6ad355a62a97b066971601d687210b3eb8
hilmaroe <32794517+hilmaroe@users.noreply.github.com>
2017-11-18 16:31:11 +0000

    uppsetning á verkefni

---
+++ script.js
@@ -0,0 +1,61 @@
+class Videos {
+  constructor() {
+    this.error = document.querySelector('.error');
+    this.main = document.querySelector('main');
+    this.new = document.querySelector('.new');
+    this.edu = document.querySelector('.edu');
+    this.ent = document.querySelector('.ent');
+    console.log(this.main);
+  }
+
+
+
+  save(title, date) {
+  //save local
+  }
+
+
+  parseDate(date, time) {
+    const endtimetest = `${date.value}T${time.value}:00`;
+    const endtime = new Date(endtimetest);
+    return endtime;
+  }
+
+
+  hideForm() {
+  }
+
+  showForm() {
+  }
+
+  createElement() {
+    for (let i = 0; i < 4; i += 1) {
+      const divs = document.createElement('div');
+      divs.setAttribute('id', `${i}box`);
+      divs.className = 'countdown__box';
+      const span1 = document.createElement('span');
+      span1.setAttribute('id', `num${i}`);
+      span1.className = 'countdown__num';
+      const span2 = document.createElement('span');
+      span2.className = 'countdown__title';
+      span2.setAttribute('id', `title${i}`);
+      divs.appendChild(span1);
+      divs.appendChild(span2);
+      this.container.appendChild(divs);
+    }
+    document.querySelector('#title0').textContent = 'Dagar';
+    document.querySelector('#title1').textContent = 'Klukkustundir';
+    document.querySelector('#title2').textContent = 'Mínútur';
+    document.querySelector('#title3').textContent = 'Sekúntur';
+  }
+
+  delete() {
+    }
+  }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const countdown = new Videos();
+  videos.load();
+});
