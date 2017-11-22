9f6cc9f8d686e1611cdff9b4aaa7dd69a0a04883
hilmaroe <32794517+hilmaroe@users.noreply.github.com>
2017-11-22 16:20:06 +0000

    bætti við vídjóum json og flr.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 class Videos {
+
   constructor() {
     this.error = document.querySelector('.error');
     this.main = document.querySelector('main');
     this.videolist = document.querySelector('.videolist');
-
   }
-
+ // Loadar upplýsingum úr videos.json  - ajax
   load() {
     var xmlhttp = new XMLHttpRequest();
     var self = this;
@@ -16,14 +16,13 @@ class Videos {
         localStorage.setItem('user', JSON.stringify(myObj));
 
         self.showData(myObj);
-    }
+    } else{  } // show Engin gögn til á síðu
   };
     xmlhttp.open("GET", "videos.json", true);
     xmlhttp.send();
   }
 
   showData(myObj){
-
     const title1 = myObj.videos[2].title;
     const catNum = myObj.categories.length;
     for (let i = 0; i<catNum; i+=1){
@@ -33,15 +32,11 @@ class Videos {
       let idArr = myObj.categories[i].videos;
 
       this.createElement(cat, fjoldi, myObj, idArr);
-
-
     }
     this.openVideo();
   }
 
-
-
-
+  //býr til categoríur og raðar réttum víjóum inn
   createElement(cat, fjoldi, myObj, idArr) {
 
     const container = document.createElement('div');
@@ -55,11 +50,8 @@ class Videos {
     container.className = "videolist__container";
     this.videolist.appendChild(container);
 
-
     for (let i = 0; i < fjoldi; i += 1) {
-
       const stak = idArr[i]-1; //raðar vídjóum eftir ID inn í categories
-
 
       const videoNum = document.createElement('div');
       videoNum.className = "videolist__video";
@@ -74,54 +66,63 @@ class Videos {
       time.textContent = this.msToTime(myObj.videos[stak].duration*1000);
       poster.appendChild(time);
 
-
       titill.className = "heading__three";
       titill.textContent = myObj.videos[stak].title;
 
       const dags = document.createElement('span');
       const img = document.createElement('img');
+
+      dags.className = "videolist__created";
+      dags.textContent = this.parseTime(myObj.videos[stak].created);
       img.className = "videolist__img";
       img.setAttribute('src', myObj.videos[stak].poster);
-
       poster.appendChild(img);
-
       videoNum.appendChild(poster);
       videoNum.appendChild(titill);
       videoNum.appendChild(dags);
-
       container.appendChild(videoNum);
     }
-
   }
-
+  // Setur Event Listner á öll vídjó
   openVideo(){
-    var a = document.getElementsByClassName('videolist__video');
-
-
+    const a = document.getElementsByClassName('videolist__video');
     for (let i = 0; i<a.length; i+=1){
       a[i].addEventListener('click', function (){
       window.location.href='video.html?id='+this.id;
-    });
+      });
+    }
   }
-
-  }
-  // Höndla Tíma!!!
+  // Time Overlay(ms)
   msToTime(lengd) {
       const min = Math.floor(lengd / 60000);
       const sek = ((lengd % 60000) / 1000).toFixed(0);
     return (sek == 60 ? (min+1) + ":00" : min + ":" + (sek < 10 ? "0" : "") + sek);
    }
+  //Created fyrir síðan
+  parseTime(created) {
+    const now = new Date();
 
-  parseDate(date, time) {
-    const endtimetest = `${date.value}T${time.value}:00`;
-    const endtime = new Date(endtimetest);
-    return endtime;
+    const tdelta = Date.parse(now) - created;
+    console.log(tdelta);
+
+    const secs = Math.floor((tdelta % (1000 * 60)) / 1000);
+    const mins = Math.floor((tdelta % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor((tdelta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const days = Math.floor(tdelta / (1000 * 60 * 60 * 24));
+    const weeks = Math.floor(days / 7);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(days/365);
+
+    let timetext;
+    if (years > 0) {timetext = "Fyrir "+years+" ári/árum síðan"
+    } else if (months > 0) {timetext = "Fyrir " +months+ " mánuði/mánuðum síðan"
+    } else if (weeks > 0){timetext = "Fyrir "+weeks+" viku/vikum síðan"
+    } else if (days > 0) {timetext = "Fyrir "+days+" degi/dögum síðan"
+    } else if (hours > 0) {timetext = "Fyrir " +hours+" klukkstund/klukkustundum síðan"}
+    else {timetext = "Fyrir minna en klukkustund síðan"}
+    return timetext;
   }
-
-
-  }
-
-
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const videos = new Videos();
